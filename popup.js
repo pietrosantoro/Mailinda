073d33bb16e757db07a95096542a129f5a50e047
Pietro Santoro <pietro.santoro2@gmail.com>
2019-06-23 21:32:20 +0100

    Revert "Revert "script request and passing data to background""
    This reverts commit e439a25cc7ccfd46c9c882ac7f3ab1c7c328d8f2.

--- popup.js
+++ popup.js
@@ -10,9 +10,24 @@ chrome.storage.sync.get('color', function(data) {
   changeColor.setAttribute('value', data.color);
 });
 */
+let bgpage = chrome.extension.getBackgroundPage(); //background page
+let request_html = bgpage.request_html;
+let string_html = String(request_html)
+
+
+//var allDiv = string_html.find("div");
+//console.log(allDiv)
 
 function test(){
   console.log("sono popup.js");
+
+
+  console.log(string_html)
+  $(string_html).appendTo('body')
+  //console.log(request_html)
+
+  // every time click in popup, new email goes to 0 and remove badge icon
+  bgpage.new_email = 0;
   chrome.browserAction.setBadgeText({text: ""});
 }
 
