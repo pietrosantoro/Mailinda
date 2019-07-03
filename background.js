290e474b14dde717514081ac9dc850fe1fbf03b3
Pietro Santoro <pietros@google.com>
2019-07-03 15:45:07 +0100

    new icon and permission updated

--- background.js
+++ background.js
@@ -38,81 +38,45 @@ function isEmpty(obj) {
   return true;
 }
 
-
 /* receive the entire html email page from script.js */
 
 function receiver(request, sender, sendResponse){
   oldEmailCounter = newEmailCounter
   newEmailCounter = 0;
   var domHTML = new DOMParser().parseFromString(request, "text/html");    //parse string request into HTML
-  // console.log(domHTML)
 
   allEmail = getJSON(domHTML);
   allEmail.splice(allEmail.length-2, 2) //clean allEmail object, delete last 2 elements
 
-  myNewEmail = [];
-  if(!isEmpty(allEmail)){
-    newEmail = allEmail.reduce(function(obj, email) {
-    if(email["Email Status"]=="New"){
-        myNewEmail.push(email)
-        
-        obj[email["Case Number"]] = (obj[email["Case Number"]] || 0) + 1;
-    }
-      return obj;
-    }, {})
-}
-
-
-var currentCase
-var casesIndexes = {}
-collapsedCases = []
-console.log(currentCase)
-allEmail.forEach((e, i) => {
-  if (!(e["Case Number"] in casesIndexes)) {
-    casesIndexes[e["Case Number"]] = i
-    currentCase = allEmail[casesIndexes[e["Case Number"]]]
-    currentCase["Emails Indexes"] = []
-    currentCase["Total Emails"] = 0
-    currentCase["New Emails"] = 0
-    currentCase["Read Emails"] = 0
-    currentCase["Sent Emails"] = 0
-    currentCase["Replied Emails"] = 0
-    console.log(currentCase)
+  var currentCase;
+  var casesNumbers = [];
+  collapsedCases = [];
+  console.log(currentCase)
+  allEmail.forEach((e, i) => {
+    if (!(casesNumbers.includes(e["Case Number"]))) {
+      casesNumbers.push(e["Case Number"])
+      e["Emails Indexes"] = []
+      e["Total Emails"] = 0
+      e["New Emails"] = 0
+      e["Read Emails"] = 0
+      e["Sent Emails"] = 0
+      e["Replied Emails"] = 0
+      collapsedCases.push(e)
     } 
-  if (e["Case Number"] in casesIndexes) {
-    let currentCase = allEmail[casesIndexes[e["Case Number"]]]
-    currentCase["Total Emails"] ++;
-    currentCase["Emails Indexes"].push(i)
-    switch(e["Email Status"]) {
-      case "New": currentCase["New Emails"] ++; break;
-      case "Read": currentCase["Read Emails"] ++; break;
-      case "Sent": currentCase["Sent Emails"] ++; break;
-      case "Replied": currentCase["Replied Emails"] ++; break;
+    if (casesNumbers.includes(e["Case Number"])) {
+      currentCase = collapsedCases[casesNumbers.indexOf(e["Case Number"])]
+      currentCase["Total Emails"] ++;
+      currentCase["Emails Indexes"].push(i)
+      switch(e["Email Status"]) {
+        case "New": currentCase["New Emails"] ++; newEmailCounter++; break;
+        case "Read": currentCase["Read Emails"] ++; break;
+        case "Sent": currentCase["Sent Emails"] ++; break;
+        case "Replied": currentCase["Replied Emails"] ++; break;
+      }
     }
-  }
-})
+  })
+  console.log(collapsedCases)
 
-Object.values(casesIndexes).forEach(e => {
-  //console.log(allEmail)
-collapsedCases.push(allEmail[e])
-})
-
-console.log(collapsedCases)
-
-
-
-
-
-
-
-  // console.log(myNewEmail)
-  // console.log(newEmail)
-  if(!isEmpty(newEmail)){
-    newEmailCounter = Object.values(newEmail).reduce((a, b) => a + b);
-  }
-  else
-    newEmailCounter=0;
-  
   if(newEmailCounter > oldEmailCounter)
     getNotification = true
 
@@ -129,7 +93,7 @@ console.log(collapsedCases)
       } 
     );
   }
-  console.log(newEmailCounter)
+  console.log("newEmail: ", newEmailCounter)
 
   request_html = request;
 
@@ -137,7 +101,7 @@ console.log(collapsedCases)
     chrome.browserAction.setBadgeText({text: ""});
   }
   else{
-    chrome.browserAction.setBadgeBackgroundColor({ color: [255, 0, 0, 255] });
+    chrome.browserAction.setBadgeBackgroundColor({ color: [255, 0, 0, 255] }); //red color badge
     chrome.browserAction.setBadgeText({text: String(newEmailCounter)});
   }
 }
--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 //content script runs after page load
 
 // http request every 30 seconds
+
+
+
+
   setInterval(function(){
   $.get("https://smbsalesimplementation--uat.cs10.my.salesforce.com/00OJ0000000uj6B", function(response) { 
       chrome.runtime.sendMessage(response)
     });
   },3000)
+
+  console.log("script.js")
