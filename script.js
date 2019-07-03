e8489fa8ff458fe7c66dd45b72c6e5c4425d3ec5
Pietro Santoro <pietros@google.com>
2019-07-03 08:27:07 +0100

    installed vue file

--- script.js
+++ script.js
@@ -5,4 +5,4 @@
   $.get("https://smbsalesimplementation--uat.cs10.my.salesforce.com/00OJ0000000uj6B", function(response) { 
       chrome.runtime.sendMessage(response)
     });
-  },30000)
+  },3000)
