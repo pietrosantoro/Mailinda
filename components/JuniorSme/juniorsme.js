acd046f7e84611e7ca6c612356ad396220e2f03a
pietros <pietros@google.com>
2020-01-02 09:14:55 +0000

    global junior sme feature

--- components/JuniorSme/juniorsme.js
+++ components/JuniorSme/juniorsme.js
@@ -1,6 +1,15 @@
 var templatejuniorsme = `
 <div id="juniorsmecomponent">
   <div class="container-fluid">
+    <div class="btn-group">
+  <button type="button" class="btn btn-danger btn-sm dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    {{current_location}}
+  </button>
+  <img class="country" v-bind:src="'images/' + this.current_location + '.png'" >
+  <div class="dropdown-menu">
+    <a v-for="location in this.all_location"class="dropdown-item" href="#" @click="change_location(location)">{{location}}</a>
+  </div>
+</div>
     <div class="table-responsive">  
       <table class="table table-hover table-dark">
         <thead>
@@ -29,9 +38,12 @@ var juniorsme = Vue.component("juniorsme", {
     return {
       count: 0,
       junior_sme_daily: "",
+      junior_sme_global: "",
       hangout_link: "",
       current_day: "",
-      current_hours: ""
+      current_hours: "",
+      all_location: "",
+      current_location: ""
     };
   },
   activated: function () {
@@ -44,16 +56,33 @@ var juniorsme = Vue.component("juniorsme", {
     var current_date = new Date();
     this.current_day = current_date.getDay()
     this.current_hours = current_date.getHours()
+    var location;
+    // everytime the component is mounted, I get location from local storage
+    chrome.storage.sync.get(['location'], function (result) {
+      if (result.location) {
+        location = result.location;
+        this.current_location = result.location;
+      }
+      else {
+        location = "Dublin"
+        this.current_location = result.location;
+      }
+
+    })
     // console.log(current_date)
     // console.log(this.current_day)
     // console.log(this.current_hours)
-    fetch('http://35.228.175.186/process_data/general-data/raw/master/junior-sme.json')
+    fetch('http://35.228.175.186/process_data/general-data/raw/master/g_junior_sme.json')
       .then(response => response.json())
       .then(data => {
         // console.log(data)
         // console.log(data.juniorsme[this.current_day])
-        this.junior_sme_daily = data.juniorsme[this.current_day]
-        this.hangout_link = data.hangout_link
+        this.junior_sme_global = data
+        this.all_location = Object.keys(data)
+        this.current_location = location
+        console.log(data[this.current_location])
+        this.junior_sme_daily = data[this.current_location].juniorsme[this.current_day]
+        this.hangout_link = data[this.current_location].hangout_link
         // console.log(this.hangout_link)
       })
       .catch(error => console.error(error))
@@ -67,6 +96,14 @@ var juniorsme = Vue.component("juniorsme", {
 
       console.log("hangout")
       window.open(this.hangout_link[agent]);
+    },
+    change_location(location) {
+      console.log(location)
+      this.current_location = location;
+      this.junior_sme_daily = this.junior_sme_global[this.current_location].juniorsme[this.current_day];
+      chrome.storage.sync.set({ location: location }, function (result) {
+        console.log("location set to " + location);
+      });
     }
   },
 });
